c850276aa260147f076e80251cf5776df0906253
Solomon Huckstep <solomonhuckstep.dev@gmail.com>
2026-07-28 16:24:43 -0400

    Controller does less now
    Moved the database checking to a service and only made the controller
    take in the request, get a return from the service, the reply with the
    result

--- PrintSpooler.Api/Controllers/PrintJobController.cs
+++ PrintSpooler.Api/Controllers/PrintJobController.cs
@@ -1,30 +1,32 @@
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
 using PrintSpooler.Api.Contracts;
 using PrintSpooler.Core.Models;
-using PrintSpooler.Infrastructure.Data;
+using PrintSpooler.Core.Services;
 
 namespace PrintSpooler.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class PrintJobController(AppDbContext dbContext) : ControllerBase
+public class PrintJobController(IJobService jobService) : ControllerBase
 {
     [HttpPost(Name = "PostPrintJob")]
     public async Task<IActionResult> Post(CreateJobRequest jobRequest)
     {
-        var job = new Job
-        {
-            Id = Guid.NewGuid(),
-            SubmittedBy = jobRequest.SubmittedBy,
-            FileName = jobRequest.FileName,
-            RawData = jobRequest.RawData,
-            ContentType = jobRequest.ContentType,
-            PrinterId = jobRequest.PrinterId,
-        };
-
-        dbContext.Jobs.Add(job);
-        await dbContext.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(Post), new { id = job.Id }, job);
+        return await jobService
+            .CreateJob(
+                new JobCreationData
+                {
+                    PrinterId = jobRequest.PrinterId,
+                    SubmittedBy = jobRequest.SubmittedBy,
+                    RawData = jobRequest.RawData,
+                    ContentType = jobRequest.ContentType,
+                    FileName = jobRequest.FileName,
+                }
+            )
+            .Match(
+                job => CreatedAtAction(nameof(Post), new { id = job.Id }, job),
+                errors => Problem(detail: errors.First().Description, statusCode: 400)
+            );
     }
 }
