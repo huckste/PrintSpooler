c040203a24b266245a2ab099dc314e223b8e2e5c
Solomon Huckstep <solomonhuckstep.dev@gmail.com>
2026-07-28 14:27:18 -0400

    added a foreignKey for printer

---
+++ PrintSpooler.Infrastructure/Migrations/20260728171833_AddPrinterForeignKey.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrintSpooler.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrinterForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_PrinterId",
+                table: "Jobs",
+                column: "PrinterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_Printers_PrinterId",
+                table: "Jobs",
+                column: "PrinterId",
+                principalTable: "Printers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_Printers_PrinterId",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_PrinterId",
+                table: "Jobs");
+        }
+    }
+}
