bc7d55fe78ff60acc1e8045675bd2b2bd500c33d
Solomon Huckstep <solomonhuckstep.dev@gmail.com>
2026-07-28 14:26:34 -0400

    moved to primary constructor

--- PrintSpooler.Infrastructure/Data/AppDbContext.cs
+++ PrintSpooler.Infrastructure/Data/AppDbContext.cs
@@ -3,11 +3,8 @@ namespace PrintSpooler.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using PrintSpooler.Core.Models;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options) { }
-
     public DbSet<Job> Jobs { get; set; }
     public DbSet<Printer> Printers { get; set; }
     public DbSet<AuditLog> AuditLogs { get; set; }
