4d42fd38f51e2ca59ec66601823d114a635bb37c
Solomon Huckstep <solomonhuckstep.dev@gmail.com>
2026-07-29 00:45:19 -0400

    enums now show as string values in db

--- PrintSpooler.Infrastructure/Data/AppDbContext.cs
+++ PrintSpooler.Infrastructure/Data/AppDbContext.cs
@@ -8,4 +8,15 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Job> Jobs { get; set; }
     public DbSet<Printer> Printers { get; set; }
     public DbSet<AuditLog> AuditLogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Job>().Property(j => j.Status).HasConversion<string>();
+
+        modelBuilder.Entity<Printer>().Property(p => p.Status).HasConversion<string>();
+
+        modelBuilder.Entity<AuditLog>().Property(a => a.Action).HasConversion<string>();
+
+        modelBuilder.Entity<AuditLog>().Property(a => a.PerformedBy).HasConversion<string>();
+    }
 }
